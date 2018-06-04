af033186b67d9a24a767b50c17ee79485fd1d13a
Tim Kurowski <timk10@comcast.net>
2018-06-04 15:42:03 -0400

    initial

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 console.log('testing')
-const button = document.querySelector('button')
-
-const content = function(){
-
-const heading = document.querySelector('h1')
-heading.textContent = 'Obsidian\'s spellbook'
-}
-
-button.addEventListener('click',content)

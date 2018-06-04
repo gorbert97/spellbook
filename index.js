bc8381b4ef2309a1b726f274ef8e05efd6c2bf89
Tim Kurowski <timk10@comcast.net>
2018-06-04 15:38:01 -0400

    step 1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('testing')
+const button = document.querySelector('button')
+
+const content = function(){
+
+const heading = document.querySelector('h1')
+heading.textContent = 'Obsidian\'s spellbook'
+}
+
+button.addEventListener('click',content)

75dfa31b340c0f9ec0b7eea5b2d7510acc56711d
Tim Kurowski <timk10@comcast.net>
2018-06-04 19:52:06 -0400

    final

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 console.log('testing')
+const button = document.querySelector('#button')
+const buttonTextBox = document.querySelector('#f')
 
+const content = function(){
+    const heading2 =document.querySelector('#daily')
+    heading2.textContent = 'spells learned today'
+
+    const heading = document.querySelector('h1')
+    heading.textContent = 'Obsidian\'s spellbook'
+}
+
+const content2 = function(){
+    const heading =document.querySelector("h1")
+    heading.textContent = head1.value
+   
+    const heading2 =document.querySelector("h2")
+    heading2.textContent = head2.value
+}
+
+button.addEventListener('click',content)
+
+buttonTextBox.addEventListener('click',content2)
+buttonTextBox.addEventListener('ctrlKey',content2)

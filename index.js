40a7aeb3c1180b048688b7b7a6a7e4debbd9a430
Tim Kurowski <timk10@comcast.net>
2018-06-05 15:57:16 -0400

    created different functions for each span, and the list

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 const form = document.querySelector('form')
-
-const changeHeading = function(ev) {
-  ev.preventDefault()
-
-  const f = ev.target
-  const spellName = f.spellName.value
-  const spellLevel = f.spellLevel.value
-  const spellDamage = f.spellDamage.value
-
-  const list = document.createElement("LI")
-  
-  const spanName = document.createElement('span');
-  spanName.style.color = "blue";
-const name = document.createTextNode(spellName)
-const level = document.createTextNode(" level " + spellLevel + " ")
-const damage = document.createTextNode(spellDamage)
-const spanDamage = document.createElement('span');
+function SpanName(spellName){
+  const spanName = document.createElement('span')
+  spanName.style.color = "blue"
+  const name = document.createTextNode(spellName)
+  spanName.appendChild(name)
+  return spanName
+}
+function SpanLevel(spellLevel){
+ // const level = document.createTextNode(" level " + spellLevel + " ")
+  const spanLevel = document.createElement('span')
+  spanLevel.style.color = "purple"
+  const level = document.createTextNode(" level "+ spellLevel+ " ")
+  spanLevel.appendChild(level)
+  return spanLevel
+}
+function SpanDamage(spellDamage){
+  const spanDamage = document.createElement('span');
 spanDamage.style.color = "red";
-
-spanName.appendChild(name);
+const damage = document.createTextNode(spellDamage)
 spanDamage.appendChild(damage);
+return spanDamage
+}
+function ulist(spanName,level,spanDamage){
+  const list = document.createElement("LI")
   list.appendChild(spanName) 
   list.appendChild(level) 
   list.appendChild(spanDamage) 
   document.querySelector("#spells").appendChild(list)
+}
+const changeHeading = function(ev) {
+  ev.preventDefault()
+  const f = ev.target
+  const spellName = f.spellName.value
+  const spellLevel = f.spellLevel.value
+  const spellDamage = f.spellDamage.value
+const name =SpanName(spellName)
+const lvl =SpanLevel(spellLevel)
+const dmg = SpanDamage(spellDamage)
+ulist(name,lvl,dmg)
   f.reset()
 }
-
-
-
 form.addEventListener('submit', changeHeading)

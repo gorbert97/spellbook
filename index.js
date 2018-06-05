5e59259ecf91bd48e17c05d5d24c05da2ee82607
Tim Kurowski <timk10@comcast.net>
2018-06-05 15:10:38 -0400

    changed the color of the input and output elements in the form

--- index.js
+++ index.js
@@ -8,11 +8,25 @@ const changeHeading = function(ev) {
   const spellLevel = f.spellLevel.value
   const spellDamage = f.spellDamage.value
 
-const list = document.createElement("LI")
-const text = document.createTextNode(spellName + " level " + spellLevel + " "  + spellDamage)
-  list.appendChild(text)     
+  const list = document.createElement("LI")
+  
+  const spanName = document.createElement('span');
+  spanName.style.color = "blue";
+const name = document.createTextNode(spellName)
+const level = document.createTextNode(" level " + spellLevel + " ")
+const damage = document.createTextNode(spellDamage)
+const spanDamage = document.createElement('span');
+spanDamage.style.color = "red";
+
+spanName.appendChild(name);
+spanDamage.appendChild(damage);
+  list.appendChild(spanName) 
+  list.appendChild(level) 
+  list.appendChild(spanDamage) 
   document.querySelector("#spells").appendChild(list)
   f.reset()
 }
+
+
 
 form.addEventListener('submit', changeHeading)

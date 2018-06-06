4e930035278f0fe9e6f03280cf0dc6a54a6d6590
Tim Kurowski <timk10@comcast.net>
2018-06-06 12:43:25 -0400

    day 3 in class

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
-const form = document.querySelector('form')
-function SpanName(spellName){
-  const spanName = document.createElement('span')
-  spanName.style.color = "blue"
-  const name = document.createTextNode(spellName)
-  spanName.appendChild(name)
-  return spanName
+const app = {
+  init: function() {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', ev => {
+      this.handleSubmit(ev)
+    })
+  },
+
+  renderProperty: function(name, value) {
+    const el = document.createElement('span')
+    el.textContent = value
+    el.classList.add(name)
+    return el
+  },
+
+  renderItem: function(spell) {
+    // ['name', 'level']
+    properties = Object.keys(spell)
+
+    // collect an array of renderProperty's return values
+    // (an array of <span> elements)
+    const childElements = properties.map(property => {
+      return this.renderProperty(property, spell[property])
+    })
+
+    const item = document.createElement('li')
+    item.classList.add('spell')
+
+    // append each <span> to the <li>
+    childElements.forEach(el => {
+      item.appendChild(el)
+    })
+
+    return item
+  },
+
+  handleSubmit: function(ev) {
+    ev.preventDefault()
+
+    const f = ev.target
+
+    const spell = {
+      name: f.spellName.value,
+      level: f.level.value,
+    }
+
+    const item = this.renderItem(spell)
+
+    const list = document.querySelector('#spells')
+    list.appendChild(item)
+
+    f.reset()
+  },
 }
-function SpanLevel(spellLevel){
- // const level = document.createTextNode(" level " + spellLevel + " ")
-  const spanLevel = document.createElement('span')
-  spanLevel.style.color = "purple"
-  const level = document.createTextNode(" level "+ spellLevel+ " ")
-  spanLevel.appendChild(level)
-  return spanLevel
-}
-function SpanDamage(spellDamage){
-  const spanDamage = document.createElement('span');
-spanDamage.style.color = "red";
-const damage = document.createTextNode(spellDamage)
-spanDamage.appendChild(damage);
-return spanDamage
-}
-function ulist(spanName,level,spanDamage){
-  const list = document.createElement("LI")
-  list.appendChild(spanName) 
-  list.appendChild(level) 
-  list.appendChild(spanDamage) 
-  document.querySelector("#spells").appendChild(list)
-}
-const changeHeading = function(ev) {
-  ev.preventDefault()
-  const f = ev.target
-  const spellName = f.spellName.value
-  const spellLevel = f.spellLevel.value
-  const spellDamage = f.spellDamage.value
-const name =SpanName(spellName)
-const lvl =SpanLevel(spellLevel)
-const dmg = SpanDamage(spellDamage)
-ulist(name,lvl,dmg)
-  f.reset()
-}
-form.addEventListener('submit', changeHeading)
+
+app.init()
